266a97bde5b5149f013d8b3e9427016cf33b98bb
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 16:39:54 -0400

    Delete Code/WGCNA/CleanedRes directory

--- Code/WGCNA/CleanedRes/Protein/DG_Preserve.R
+++
@@ -1,9 +0,0 @@
-source("/data/humgen/daskalakislab/dipietro/SciencePaper/Code/WGCNA/CleanedRes/WGCNA_CleanedResProt.R")
-
-data_DG <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/CleanData/Protein/Proteins/DG/Expression_Y123_residuals.RDS")
-
-
-preservation(data = data_DG,
-             region = "DG", 
-             out_dir = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/WGCNA/CleanedRes/Protein/DG/", 
-             power = 12)
