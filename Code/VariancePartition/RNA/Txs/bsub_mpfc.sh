75529ca194e2859bee2ffc53023340e13dcc968e
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:29:30 -0400

    Delete Code/VariancePartition/RNA/Txs directory

--- Code/VariancePartition/RNA/Txs/bsub_mpfc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Txs/logs/out_vp12_mpfc.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Txs/logs/err_vp12_mpfc.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J TXVPmPFC
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Txs/vp_mpfc.R
