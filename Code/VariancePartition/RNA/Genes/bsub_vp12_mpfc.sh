2dc33187c6704b09d10e891a920b188c6a605088
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:32:08 -0400

    Delete Code/VariancePartition/RNA/Genes/bsub_vp12_mpfc.sh

--- Code/VariancePartition/RNA/Genes/bsub_vp12_mpfc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/logs/out_vp12_mpfc.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/logs/err_vp12_mpfc.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J VPmPFC
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/VarPartY1Y2_mPFC.R
