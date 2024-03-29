c24867c14b9a03fc8542686f88498fd89d073fe2
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:27:11 -0400

    Delete Code/VariancePartition/Protein/PCAs directory

--- Code/VariancePartition/Protein/PCAs/bsub_ca.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Protein/PCAs/logs/out_vp12_ca.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Protein/PCAs/logs/err_vp12_ca.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J ProCA
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Protein/PCAs/vp_ca.R
