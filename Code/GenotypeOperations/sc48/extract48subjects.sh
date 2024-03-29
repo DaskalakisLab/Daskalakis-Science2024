1b48fa8d9c1742bf0596dc7e9092d6d27810e70d
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:11:43 -0400

    Delete Code/GenotypeOperations/sc48 directory

--- Code/GenotypeOperations/sc48/extract48subjects.sh
+++
@@ -1,8 +0,0 @@
-plink=/data/humgen/daskalakislab/dipietro/Tools/PLINK/plink2
-geno_file=/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Genotype/sc48_PlinkFormat/kleinman_single_cell_n82.maf01
-out_file=/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Genotype/sc48_PlinkFormat/kleinman_single_cell_n48.maf01
-subjects=/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Genotype/sc48/sc48_subjects.txt
-
-$plink --bfile $geno_file --keep $subjects --make-bed --out $out_file
-
-mv ${out_file}.log /data/humgen/daskalakislab/dipietro/SciencePaper/Data/Genotype/sc48_PlinkFormat/logs
--- Code/GenotypeOperations/sc48/updateRSIDs.sh
+++
@@ -1,8 +0,0 @@
-plink=/data/humgen/daskalakislab/dipietro/Tools/PLINK/plink2
-geno_file=/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Genotype/sc48_PlinkFormat/kleinman_single_cell_n48.maf01
-out_file=/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Genotype/sc48_PlinkFormat/kleinman_single_cell_n48.maf01.rsids
-rsids=/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Genotype/rsidMap_kleinman_single_cell_n82.maf01.txt
-
-$plink --bfile $geno_file --update-name $rsids --make-bed --out $out_file
-
-mv ${out_file}.log /data/humgen/daskalakislab/dipietro/SciencePaper/Data/Genotype/sc48_PlinkFormat/logs
