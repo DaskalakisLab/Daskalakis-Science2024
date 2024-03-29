6b08e5bf169caf30b2e1ada2fe0fbf100776bac5
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 16:41:34 -0400

    Delete Code/GenericFunctions directory

--- Code/GenericFunctions/ExtractGenes/ExtractionFunctions/LoadFunctions.R
+++
@@ -1,7 +0,0 @@
-extract_dir <- "/data/humgen/daskalakislab/dipietro/SciencePaper/Code/GenericFunctions/ExtractGenes/ExtractionFunctions/"
-extraction_functions <- list.files(extract_dir)
-extraction_functions <- extraction_functions[!extraction_functions=="LoadFunctions.R"]
-for (f in extraction_functions){
-  source(paste0(extract_dir,f))
-}
-rm(extract_dir,extraction_functions,f)
