7f9a623a461ed05861f821da724f442867e38ec8
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:18:27 -0400

    Delete Code/VariancePartition/RNA/Genes/PCA directory

--- Code/VariancePartition/RNA/Genes/PCA/bsub_ca.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/PCA/logs/out_vp12_ca.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/PCA/logs/err_vp12_ca.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J VPCA
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/PCA/vp_ca.R
