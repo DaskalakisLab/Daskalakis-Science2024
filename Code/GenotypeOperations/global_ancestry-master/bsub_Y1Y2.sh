3976a444a89b9001f7ff778eaadd9806fc7332bd
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:13:34 -0400

    Delete Code/GenotypeOperations/global_ancestry-master/bsub_Y1Y2.sh

--- Code/GenotypeOperations/global_ancestry-master/bsub_Y1Y2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/GenotypeOperations/global_ancestry-master/logs/out_global_ancestry_Y1Y2.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/GenotypeOperations/global_ancestry-master/logs/err_global_ancestry_Y1Y2.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J GAY1Y2
-bash /data/humgen/daskalakislab/dipietro/SciencePaper/Code/GenotypeOperations/global_ancestry-master/ancestry_pipeline_Y1Y2.sh
