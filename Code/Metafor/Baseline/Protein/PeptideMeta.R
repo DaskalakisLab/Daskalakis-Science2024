54a39f0a6ebb6bc390d46a17e8ea5403d84c0709
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 16:48:14 -0400

    Update PeptideMeta.R

--- Code/Metafor/Baseline/Protein/PeptideMeta.R
+++ Code/Metafor/Baseline/Protein/PeptideMeta.R
@@ -1,66 +1,66 @@
 source("/data/humgen/daskalakislab/dipietro/SciencePaper/Code/Metafor/metafor_cc.R")
 
 # MDD
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/MDD/results_MDD_CentralAmyg_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/MDD/results_MDD_CentralAmyg_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/MDD/results_MDD_CentralAmyg_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/MDD/results_MDD_CentralAmyg_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/MDD_CentralAmygcc.RDS",
              type="peptide")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/MDD/results_MDD_DG_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/MDD/results_MDD_DG_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/MDD/results_MDD_DG_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/MDD/results_MDD_DG_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/MDD_DGcc.RDS",
              type="peptide")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/MDD/results_MDD_mPFC_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/MDD/results_MDD_mPFC_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/MDD/results_MDD_mPFC_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/MDD/results_MDD_mPFC_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/MDD_mPFCcc.RDS",
              type="peptide")
 
 # PTSD
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD/results_PTSD_CentralAmyg_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/PTSD/results_PTSD_CentralAmyg_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD/results_PTSD_CentralAmyg_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/PTSD/results_PTSD_CentralAmyg_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/PTSD_CentralAmygcc.RDS",
              type="peptide")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD/results_PTSD_DG_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/PTSD/results_PTSD_DG_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD/results_PTSD_DG_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/PTSD/results_PTSD_DG_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/PTSD_DGcc.RDS",
              type="peptide")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD/results_PTSD_mPFC_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/PTSD/results_PTSD_mPFC_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD/results_PTSD_mPFC_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/PTSD/results_PTSD_mPFC_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/PTSD_mPFCcc.RDS",
              type="peptide")
 
 # PTSD + MDD
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD_MDD/results_PTSD+MDD_CentralAmyg_PTSD_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/PTSD_MDD/results_PTSD+MDD_CentralAmyg_PTSD_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD_MDD/results_PTSD+MDD_CentralAmyg_PTSD_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/PTSD_MDD/results_PTSD+MDD_CentralAmyg_PTSD_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/PTSD_MDD_CentralAmygcc.RDS",
              type="peptide")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD_MDD/results_PTSD+MDD_DG_PTSD_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/PTSD_MDD/results_PTSD+MDD_DG_PTSD_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD_MDD/results_PTSD+MDD_DG_PTSD_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/PTSD_MDD/results_PTSD+MDD_DG_PTSD_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/PTSD_MDD_DGcc.RDS",
              type="peptide")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD_MDD/results_PTSD+MDD_mPFC_PTSD_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/PTSD_MDD/results_PTSD+MDD_mPFC_PTSD_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD_MDD/results_PTSD+MDD_mPFC_PTSD_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/PTSD_MDD/results_PTSD+MDD_mPFC_PTSD_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/PTSD_MDD_mPFCcc.RDS",
              type="peptide")
 
 # PTSD vs MDD
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD_vs_MDD/results_PTSD_vs_MDD_CentralAmyg_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/PTSD_vs_MDD/results_PTSD_vs_MDD_CentralAmyg_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD_vs_MDD/results_PTSD_vs_MDD_CentralAmyg_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/PTSD_vs_MDD/results_PTSD_vs_MDD_CentralAmyg_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/PTSD_vs_MDD_CentralAmygcc.RDS",
              type="peptide")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD_vs_MDD/results_PTSD_vs_MDD_DG_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/PTSD_vs_MDD/results_PTSD_vs_MDD_DG_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD_vs_MDD/results_PTSD_vs_MDD_DG_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/PTSD_vs_MDD/results_PTSD_vs_MDD_DG_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/PTSD_vs_MDD_DGcc.RDS",
              type="peptide")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD_vs_MDD/results_PTSD_vs_MDD_mPFC_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y3/PTSD_vs_MDD/results_PTSD_vs_MDD_mPFC_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD_vs_MDD/results_PTSD_vs_MDD_mPFC_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch2/PTSD_vs_MDD/results_PTSD_vs_MDD_mPFC_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/Protein/Peptides/PTSD_vs_MDD_mPFCcc.RDS",
              type="peptide")
 
