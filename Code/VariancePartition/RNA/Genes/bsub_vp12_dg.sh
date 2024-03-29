c13cb6f62c9a2c886ef38543fce11124ea6c38c5
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:32:02 -0400

    Delete Code/VariancePartition/RNA/Genes/bsub_vp12_dg.sh

--- Code/VariancePartition/RNA/Genes/bsub_vp12_dg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/logs/out_vp12_dg.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/logs/err_vp12_dg.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J VPDG
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/VarPartY1Y2_DG.R
