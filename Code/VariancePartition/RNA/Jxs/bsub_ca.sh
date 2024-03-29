1ead532e07917da71f2abd37af3cfc506e1ace1f
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:29:22 -0400

    Delete Code/VariancePartition/RNA/Jxs directory

--- Code/VariancePartition/RNA/Jxs/bsub_ca.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Jxs/logs/out_vp12_ca.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Jxs/logs/err_vp12_ca.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J JXVPCA
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Jxs/vp_ca.R
--- Code/VariancePartition/RNA/Jxs/bsub_mpfc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Jxs/logs/out_vp12_mpfc.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Jxs/logs/err_vp12_mpfc.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J JXVPmPFC
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Jxs/vp_mpfc.R
