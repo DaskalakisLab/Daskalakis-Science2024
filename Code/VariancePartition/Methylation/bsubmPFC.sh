d7c3201b199eb9a19e4a031b28f8844d221e5fa2
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:33:59 -0400

    Delete Code/VariancePartition/Methylation/bsubmPFC.sh

--- Code/VariancePartition/Methylation/bsubmPFC.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Methylation/logs/out_vp12_mpfc.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Methylation/logs/err_vp12_mpfc.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J MethmPFC
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Methylation/vp_mpfc.R
