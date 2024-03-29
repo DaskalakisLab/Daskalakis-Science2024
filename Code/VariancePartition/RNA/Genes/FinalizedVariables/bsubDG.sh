d8e25843fc497489eee72c84ceaad12ff8e0e088
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:30:21 -0400

    Delete Code/VariancePartition/RNA/Genes/FinalizedVariables directory

--- Code/VariancePartition/RNA/Genes/FinalizedVariables/bsubDG.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/FinalizedVariables/logs/out_vp12_dg.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/FinalizedVariables/logs/err_vp12_dg.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J VPDG
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/RNA/Genes/FinalizedVariables/VPDG.R
