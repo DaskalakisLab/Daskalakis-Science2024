d2cb62bc7ace9ae6c6bea935585b345d65c70d8a
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:33:50 -0400

    Delete Code/VariancePartition/Methylation/bsubDG.sh

--- Code/VariancePartition/Methylation/bsubDG.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Methylation/logs/out_vp12_dg.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Methylation/logs/err_vp12_dg.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J MethDG
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Methylation/vp_dg.R
