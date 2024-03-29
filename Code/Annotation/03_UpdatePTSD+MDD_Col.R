01686e9edd734b29ffb8c29e6646fa12cb6b578a
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 16:01:33 -0400

    Delete Code/Annotation directory

--- Code/Annotation/03_UpdatePTSD+MDD_Col.R
+++
@@ -1,9 +0,0 @@
-anno12 <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_Y1Y2.RDS")
-anno3 <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_Y3.RDS")
-
-anno12$PTSD_MDD <- anno12$PTSD + anno12$MDD
-
-anno3$PTSD_MDD <- anno3$PTSD + anno3$MDD
-
-saveRDS(anno12, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_Y1Y2.RDS")
-saveRDS(anno3, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_Y3.RDS")
--- Code/Annotation/README.md
+++
@@ -1,4 +0,0 @@
-Code used to create annotation (metadata) RDS objects for Y1Y2 and Y3 R01 subjects.
-Codes are numbered in the order in which I created the scripts. Data fields were
-added to the annotation files incrementally as decisions about what to include in
-the paper were made.
