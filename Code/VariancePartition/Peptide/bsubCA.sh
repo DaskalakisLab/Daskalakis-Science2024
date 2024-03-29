242e59d0509a8cb28cccb82728d5e1b0ce70ab1e
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:25:51 -0400

    Delete Code/VariancePartition/Peptide/bsubCA.sh

--- Code/VariancePartition/Peptide/bsubCA.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Peptide/logs/out_vp12_ca.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Peptide/logs/err_vp12_ca.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J PepCA
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/VariancePartition/Peptide/VPCA.R
