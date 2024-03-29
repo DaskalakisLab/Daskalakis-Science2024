f176056366ae4f0c9a78f2540fd21c7cb656b7a5
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 19:39:37 -0400

    Update Limma.R

--- Code/Protein_Y12_Y3/Peptides/Y1Y2/Limma.R
+++ Code/Protein_Y12_Y3/Peptides/Y1Y2/Limma.R
@@ -1,11 +1,11 @@
 source("/data/humgen/daskalakislab/dipietro/SciencePaper/Code/Protein/Limma_Functions.R")
-sink("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/logY1Y2.txt")
+sink("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/logbatch1.txt")
 
 regions <- c("CentralAmyg", "DG", "mPFC")
 
 for (region in regions){
-  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_Y1Y2.RDS", 
-                paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/Y1Y2/Peptides/CleanWithSampleIDs/Y1Y2_",region,"_clean_no_GIS.RDS"), 
+  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_batch1.RDS", 
+                paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1/Peptides/CleanWithSampleIDs/batch1_",region,"_clean_no_GIS.RDS"), 
                 region = region, 
                 dx="PTSD", 
                 ptsd_vs_mdd = F, 
@@ -13,10 +13,10 @@ for (region in regions){
                 type = "peptide",
                 covariates = c("PTSD", "AgeDeath","Sex","PMI","ancestryPC1","ancestryPC2","Astro","inhib","ex","Oligo","Micro","OPC","Tcell","PepPC1","PepPC2","PepPC3","PepPC4","PepPC5","mitoMapped"), 
                 contrast_var = "PTSD", 
-                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD/")
+                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD/")
   
-  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_Y1Y2.RDS", 
-                paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/Y1Y2/Peptides/CleanWithSampleIDs/Y1Y2_",region,"_clean_no_GIS.RDS"), 
+  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_batch1.RDS", 
+                paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1/Peptides/CleanWithSampleIDs/batch1_",region,"_clean_no_GIS.RDS"), 
                 region = region, 
                 dx="MDD", 
                 ptsd_vs_mdd = F, 
@@ -24,10 +24,10 @@ for (region in regions){
                 type = "peptide",
                 covariates = c("MDD", "AgeDeath","Sex","PMI","ancestryPC1","ancestryPC2","Astro","inhib","ex","Oligo","Micro","OPC","Tcell","PepPC1","PepPC2","PepPC3","PepPC4","PepPC5","mitoMapped"), 
                 contrast_var = "MDD", 
-                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/MDD/")
+                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/MDD/")
   
-  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_Y1Y2.RDS", 
-                paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/Y1Y2/Peptides/CleanWithSampleIDs/Y1Y2_",region,"_clean_no_GIS.RDS"), 
+  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_batch1.RDS", 
+                paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1/Peptides/CleanWithSampleIDs/batch1_",region,"_clean_no_GIS.RDS"), 
                 region = region, 
                 dx=NULL, 
                 ptsd_vs_mdd = F, 
@@ -35,10 +35,10 @@ for (region in regions){
                 type = "peptide",
                 covariates = c("PTSD_MDD", "AgeDeath","Sex","PMI","ancestryPC1","ancestryPC2","Astro","inhib","ex","Oligo","Micro","OPC","Tcell","PepPC1","PepPC2","PepPC3","PepPC4","PepPC5","mitoMapped"), 
                 contrast_var = "PTSD_MDD", 
-                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD_MDD/")
+                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD_MDD/")
   
-  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_Y1Y2.RDS", 
-                paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/Y1Y2/Peptides/CleanWithSampleIDs/Y1Y2_",region,"_clean_no_GIS.RDS"), 
+  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_batch1.RDS", 
+                paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1/Peptides/CleanWithSampleIDs/batch1_",region,"_clean_no_GIS.RDS"), 
                 region = region, 
                 dx=NULL, 
                 ptsd_vs_mdd = T, 
@@ -46,6 +46,6 @@ for (region in regions){
                 type = "peptide",
                 covariates = c("PTSD", "AgeDeath","Sex","PMI","ancestryPC1","ancestryPC2","Astro","inhib","ex","Oligo","Micro","OPC","Tcell","PepPC1","PepPC2","PepPC3","PepPC4","PepPC5","mitoMapped"), 
                 contrast_var = "PTSD", 
-                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/Y1Y2/PTSD_vs_MDD/")
+                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Protein/Peptides/batch1/PTSD_vs_MDD/")
 }
 sink()
