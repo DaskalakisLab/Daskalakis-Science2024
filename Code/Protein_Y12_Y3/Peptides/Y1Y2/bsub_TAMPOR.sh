f1b67eee9beeb3e470b5cc81912c93947709a4f0
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 17:50:01 -0400

    Delete Code/Protein_Y12_Y3/Peptides/Y1Y2/bsub_TAMPOR.sh

--- Code/Protein_Y12_Y3/Peptides/Y1Y2/bsub_TAMPOR.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#BSUB -o /data/humgen/daskalakislab/dipietro/SciencePaper/Code/Protein/Peptides/logs/out_tampor.txt
-#BSUB -e /data/humgen/daskalakislab/dipietro/SciencePaper/Code/Protein/Peptides/logs/err_tampor.txt
-#BSUB -W 100:30
-#BSUB -q bigmem
-#BSUB -M 64000
-#BSUB -J tampor
-Rscript /data/humgen/daskalakislab/dipietro/SciencePaper/Code/Protein/Peptides/TAMPORY1Y2.R
