cba0bc36ed3ee52581f9b27947e4923467943bb6
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 16:24:27 -0400

    Update GenesMeta.R

--- Code/Metafor/Baseline/RNA/GenesMeta.R
+++ Code/Metafor/Baseline/RNA/GenesMeta.R
@@ -1,66 +1,66 @@
 source("/data/humgen/daskalakislab/dipietro/SciencePaper/Code/Metafor/metafor.R")
 
 # MDD
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/MDD/results_MDD_CentralAmyg_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/MDD/results_MDD_CentralAmyg_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/MDD/results_MDD_CentralAmyg_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/MDD/results_MDD_CentralAmyg_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/MDD_CentralAmyg.RDS",
              type="gene")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/MDD/results_MDD_DG_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/MDD/results_MDD_DG_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/MDD/results_MDD_DG_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/MDD/results_MDD_DG_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/MDD_DG.RDS",
              type="gene")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/MDD/results_MDD_mPFC_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/MDD/results_MDD_mPFC_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/MDD/results_MDD_mPFC_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/MDD/results_MDD_mPFC_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/MDD_mPFC.RDS",
              type="gene")
 
 # PTSD
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/PTSD/results_PTSD_CentralAmyg_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/PTSD/results_PTSD_CentralAmyg_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/PTSD/results_PTSD_CentralAmyg_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/PTSD/results_PTSD_CentralAmyg_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/PTSD_CentralAmyg.RDS",
              type="gene")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/PTSD/results_PTSD_DG_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/PTSD/results_PTSD_DG_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/PTSD/results_PTSD_DG_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/PTSD/results_PTSD_DG_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/PTSD_DG.RDS",
              type="gene")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/PTSD/results_PTSD_mPFC_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/PTSD/results_PTSD_mPFC_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/PTSD/results_PTSD_mPFC_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/PTSD/results_PTSD_mPFC_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/PTSD_mPFC.RDS",
              type="gene")
 
 # PTSD + MDD
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/PTSD_MDD/results_PTSD+MDD_CentralAmyg_PTSD_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/PTSD_MDD/results_PTSD+MDD_CentralAmyg_PTSD_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/PTSD_MDD/results_PTSD+MDD_CentralAmyg_PTSD_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/PTSD_MDD/results_PTSD+MDD_CentralAmyg_PTSD_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/PTSD_MDD_CentralAmyg.RDS",
              type="gene")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/PTSD_MDD/results_PTSD+MDD_DG_PTSD_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/PTSD_MDD/results_PTSD+MDD_DG_PTSD_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/PTSD_MDD/results_PTSD+MDD_DG_PTSD_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/PTSD_MDD/results_PTSD+MDD_DG_PTSD_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/PTSD_MDD_DG.RDS",
              type="gene")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/PTSD_MDD/results_PTSD+MDD_mPFC_PTSD_MDD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/PTSD_MDD/results_PTSD+MDD_mPFC_PTSD_MDD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/PTSD_MDD/results_PTSD+MDD_mPFC_PTSD_MDD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/PTSD_MDD/results_PTSD+MDD_mPFC_PTSD_MDD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/PTSD_MDD_mPFC.RDS",
              type="gene")
 
 # PTSD vs MDD
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/PTSD_vs_MDD/results_PTSD_vs_MDD_CentralAmyg_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/PTSD_vs_MDD/results_PTSD_vs_MDD_CentralAmyg_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/PTSD_vs_MDD/results_PTSD_vs_MDD_CentralAmyg_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/PTSD_vs_MDD/results_PTSD_vs_MDD_CentralAmyg_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/PTSD_vs_MDD_CentralAmyg.RDS",
              type="gene")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/PTSD_vs_MDD/results_PTSD_vs_MDD_DG_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/PTSD_vs_MDD/results_PTSD_vs_MDD_DG_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/PTSD_vs_MDD/results_PTSD_vs_MDD_DG_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/PTSD_vs_MDD/results_PTSD_vs_MDD_DG_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/PTSD_vs_MDD_DG.RDS",
              type="gene")
 
-meta_analyze(y1y2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y1Y2/PTSD_vs_MDD/results_PTSD_vs_MDD_mPFC_PTSD.RDS",
-             y3_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/Y3/PTSD_vs_MDD/results_PTSD_vs_MDD_mPFC_PTSD.RDS",
+meta_analyze(batch1_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch1/PTSD_vs_MDD/results_PTSD_vs_MDD_mPFC_PTSD.RDS",
+             batch2_file="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/RNA/Genes/batch2/PTSD_vs_MDD/results_PTSD_vs_MDD_mPFC_PTSD.RDS",
              outfile="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Metafor/Baseline/RNA/Genes/PTSD_vs_MDD_mPFC.RDS",
              type="gene")
 
