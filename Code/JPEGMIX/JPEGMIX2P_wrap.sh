b87635acf7f8d859deff890c1a69b76013156fbe
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-30 10:26:52 -0400

    Update JPEGMIX2P_wrap.sh

--- Code/JPEGMIX/JPEGMIX2P_wrap.sh
+++ Code/JPEGMIX/JPEGMIX2P_wrap.sh
@@ -1,12 +1,12 @@
 # -S /bin/bash
-#$ -e /data/ajajoo/PEC/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.e
-#$ -o /data/ajajoo/PEC/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.o
+#$ -e /path/to/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.e
+#$ -o /path/to/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.o
 # -N MDD_PGCHwMDD_MVP
 # cd to the directory from which I submitted the
 # job.  Otherwise it will execute in my data directory.
 echo Beginning to run R job
 date
-/data/cchatzinakos/anaconda3/envs/r_env/bin/R CMD BATCH --no-restore --no-save --no-readline -iseed=1 -fr.aa=/data/ajajoo/PEC/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.genes.txt -fr.gg=/data/ajajoo/PEC/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.corr.txt -fr.bb=/data/ajajoo/PEC/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.agn.txt -fr.cc=/data/ajajoo/ASHG_TWAS/GWAS/MDD/JEPEG/MDD_PGCHwMDD_MVP.txt -fr.dd=/data/ajajoo/PEC/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.dir.txt -fr.ee=/data/ajajoo/PEC/PECJPEGMIX/ResultsAllcisWithPathway202306//logs/tpath_MDD_PGCHwMDD_MVP.txt -fr.ss=/data/ajajoo/PEC/PECJPEGMIX/ResultsAllcisWithPathway202306//logs/ttpath_MDD_PGCHwMDD_MVP.txt -nsim=2 -out=/data/ajajoo/PEC/PECJPEGMIX/ResultsAllcisWithPathway202306//logs/results_MDD_PGCHwMDD_MVP.txt /data/ajajoo/PEC/PECJPEGMIX/script/20230628/callingJpegAllcisWithPathway202306.txt &> /data/ajajoo/PEC/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.Rout
+/path/to/anaconda3/envs/r_env/bin/R CMD BATCH --no-restore --no-save --no-readline -iseed=1 -fr.aa=/path/to/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.genes.txt -fr.gg=/path/to/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.corr.txt -fr.bb=/path/to/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.agn.txt -fr.cc=/data/ajajoo/ASHG_TWAS/GWAS/MDD/JEPEG/MDD_PGCHwMDD_MVP.txt -fr.dd=/path/to/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.dir.txt -fr.ee=/path/to/PECJPEGMIX/ResultsAllcisWithPathway202306//logs/tpath_MDD_PGCHwMDD_MVP.txt -fr.ss=/path/to/PECJPEGMIX/ResultsAllcisWithPathway202306//logs/ttpath_MDD_PGCHwMDD_MVP.txt -nsim=2 -out=/path/to/PECJPEGMIX/ResultsAllcisWithPathway202306//logs/results_MDD_PGCHwMDD_MVP.txt /path/to/PECJPEGMIX/script/20230628/callingJpegAllcisWithPathway202306.txt &> /path/to/PECJPEGMIX/ResultsAllcisWithPathway202306//MDD_PGCHwMDD_MVP.Rout
 date
 sleep 120
 
