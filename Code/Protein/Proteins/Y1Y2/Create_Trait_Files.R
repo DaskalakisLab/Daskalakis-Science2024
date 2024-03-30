85a799f8295c79b4b65b367a8f2858cfa140463e
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-30 11:06:41 -0400

    Update Create_Trait_Files.R

--- Code/Protein/Proteins/Y1Y2/Create_Trait_Files.R
+++ Code/Protein/Proteins/Y1Y2/Create_Trait_Files.R
@@ -1,4 +1,4 @@
-### RUN /data/humgen/daskalakislab/dipietro/SciencePaper/Code/PDabundanceLoader/Loader script before creating trait files
+### RUN /path/to//Code/PDabundanceLoader/Loader script before creating trait files
 
 library(limma)
 library(vsn)
@@ -6,9 +6,9 @@ library(doParallel)
 library(ggplot2)
 library(ggpubr)
 
-dat <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1.TMT_rawAbundances.NoCorrection.RDS")
+dat <- readRDS("/path/to//Data/Protein/TamporNormalized/batch1.TMT_rawAbundances.NoCorrection.RDS")
 dat <- as.matrix(dat)
-traits <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/Proteomics/batch_designs.RDS")
+traits <- readRDS("/path/to//Data/Protein/Proteomics/batch_designs.RDS")
 traits$Plate <- gsub("F", "", traits$Plate)
 traits$Plate <- sprintf("%02d", as.numeric(traits$Plate))
 rownames(traits) <- paste0("b",traits$Plate, ".", traits$Position)
@@ -16,13 +16,13 @@ names(traits)[names(traits)=="Plate"] <- "Batch"
 
 
 traits_Amygdala <- traits[(traits$Brain_Region=="Amygdala") | (traits$Brain_Region=="GIS"),]
-saveRDS(traits_Amygdala, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TraitFiles/traits_Amygdala.RDS")
+saveRDS(traits_Amygdala, "/path/to//Data/Protein/TraitFiles/traits_Amygdala.RDS")
 
 traits_Hippocampus <- traits[(traits$Brain_Region=="Hippocampus") | (traits$Brain_Region=="GIS"),]
-saveRDS(traits_Hippocampus, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TraitFiles/traits_Hippocampus.RDS")
+saveRDS(traits_Hippocampus, "/path/to//Data/Protein/TraitFiles/traits_Hippocampus.RDS")
 
 traits_mPFC <- traits[(traits$Brain_Region=="mPFC") | (traits$Brain_Region=="GIS"),]
-saveRDS(traits_mPFC, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TraitFiles/traits_mPFC.RDS")
+saveRDS(traits_mPFC, "/path/to//Data/Protein/TraitFiles/traits_mPFC.RDS")
 
 dat <- as.data.frame(dat)
 dat_Amygdala <- dat[, names(dat) %in% rownames(traits_Amygdala),]
@@ -33,7 +33,7 @@ dat_Amygdala <- as.matrix(dat_Amygdala)
 dat_Hippocampus <- as.matrix(dat_Hippocampus)
 dat_mPFC <- as.matrix(dat_mPFC)
 
-saveRDS(dat_Amygdala, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporDataFiles/dat_Amygdala.RDS")
-saveRDS(dat_Hippocampus, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporDataFiles/dat_Hippocampus.RDS")
-saveRDS(dat_mPFC, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporDataFiles/dat_mPFC.RDS")
+saveRDS(dat_Amygdala, "/path/to//Data/Protein/TamporDataFiles/dat_Amygdala.RDS")
+saveRDS(dat_Hippocampus, "/path/to//Data/Protein/TamporDataFiles/dat_Hippocampus.RDS")
+saveRDS(dat_mPFC, "/path/to//Data/Protein/TamporDataFiles/dat_mPFC.RDS")
 
