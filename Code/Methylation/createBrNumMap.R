950f5f4fa0dae8806084e825f17ce50740b29317
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-30 10:54:37 -0400

    Update createBrNumMap.R

--- Code/Methylation/createBrNumMap.R
+++ Code/Methylation/createBrNumMap.R
@@ -1,6 +1,6 @@
 # https://www.bioconductor.org/packages/release/workflows/vignettes/methylationArrayAnalysis/inst/doc/methylationArrayAnalysis.html
 # Code and comments by Aarti Jajoo
-setwd('/data/humgen/daskalakislab/jajoo/TACC/DNAmethylation/NormalizedData/')
+setwd('/path/to/working/dir/')
 # adding path to packages 
 #.libPaths(c(.libPaths(),"/home/ajajoo/R/x86_64-pc-linux-gnu-library/3.6/"))
 ####---Load Libraries----####
@@ -47,21 +47,21 @@ head(annEPIC)
 
 ####----Uncomment below to see preprocessing of data and QC----####
 ####----Get idat files and sample sheet and correcting names and other features----####
-baseDir <- "/data/humgen/daskalakislab/jajoo/TACC/DNAmethylation/RawData/ImageData/"
-targets <- read.metharray.sheet('/data/humgen/daskalakislab/jajoo/TACC/DNAmethylation/metadata',pattern = "methylation_data_dictionary.csv")
+baseDir <- "/path/to/ImageData/"
+targets <- read.metharray.sheet('/path/to/metadata',pattern = "methylation_data_dictionary.csv")
 Basename <- gsub("_.[Red,Grn].*","",list.files(baseDir,pattern = ".idat",recursive = TRUE))
 colnames(targets)[c(6,5)] <- c("Sentrix.ID","Sentrix.Position")
 targets$Basename <- paste0(baseDir,targets$Sentrix.ID,"/",targets$Sentrix.ID,"_",targets$Sentrix.Position)
 
 names(targets)[1] <- "Sample_Group"
 
-JoelPTDSY2 <- read.csv('/data/humgen/daskalakislab/jajoo/TACC/DNAmethylation/metadata/Joel_PTSD_R01_Y2_DNA_Packing_Slips_to_FC_12-17-2020.csv',stringsAsFactors = FALSE,header = TRUE,sep=",")
+JoelPTDSY2 <- read.csv('/path/to/Joel_PTSD_R01_Y2_DNA_Packing_Slips_to_FC_12-17-2020.csv',stringsAsFactors = FALSE,header = TRUE,sep=",")
 Jcolname <- JoelPTDSY2[1,]
 JoelPTDSY2 <- JoelPTDSY2[-1,]
 colnames(JoelPTDSY2)[c(1,8:12)] <- c("name",Jcolname[8:12]) 
 JoelPTDSY2 <- JoelPTDSY2[,c(8,14,5,9,10,11,13)]
 
-methDicDemo <- read.csv('/data/humgen/daskalakislab/jajoo/TACC/DNAmethylation/methylation_data_dictionary_with_some_demographic_data.csv',stringsAsFactors = FALSE,header = TRUE,sep=",")
+methDicDemo <- read.csv('/path/to//methylation_data_dictionary_with_some_demographic_data.csv',stringsAsFactors = FALSE,header = TRUE,sep=",")
 methDicDemo <- methDicDemo[,c(1,2,8,9,10,14)]
 
 metaDNA <- full_join(JoelPTDSY2,methDicDemo,by=c("X.11"="X"))
@@ -83,4 +83,4 @@ head(methmap)
 names(methmap) <- c("BrNum", "Brain_Region", "MethID")
 methmap[methmap$Brain_Region=="Amygdala", "Brain_Region"] <- "CentralAmyg"
 methmap[methmap$Brain_Region=="Hippocampus", "Brain_Region"] <- "DG"
-saveRDS(methmap, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Methylation/BrNumMapping/Y1Y2BrNumMap.RDS")
+saveRDS(methmap, "/path/to/result/")
