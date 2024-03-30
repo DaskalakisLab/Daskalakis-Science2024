d988c7a74b6bd560899f5e2050be3ea9a40946d3
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-30 11:07:42 -0400

    Update Tampor_batch1.R

--- Code/Protein/Proteins/Y1Y2/Tampor_batch1.R
+++ Code/Protein/Proteins/Y1Y2/Tampor_batch1.R
@@ -4,10 +4,10 @@ library(doParallel)
 library(ggplot2)
 library(ggpubr)
 
-source("/data/humgen/daskalakislab/dipietro/SciencePaper/Code/TAMPOR/TAMPOR.R")
-dat <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1/Proteins/batch1.TMT_rawAbundances.NoCorrection.RDS")
+source("/path/to/Code/TAMPOR/TAMPOR.R")
+dat <- readRDS("/path/to/Data/Protein/TamporNormalized/batch1/Proteins/batch1.TMT_rawAbundances.NoCorrection.RDS")
 dat <- as.matrix(dat)
-traits <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/Proteomics/batch_designs.RDS")
+traits <- readRDS("/path/to/Data/Protein/Proteomics/batch_designs.RDS")
 traits$Plate <- gsub("F", "", traits$Plate)
 traits$Plate <- sprintf("%02d", as.numeric(traits$Plate))
 rownames(traits) <- paste0("b",traits$Plate, ".", traits$Position)
@@ -20,7 +20,7 @@ dat<-apply(dat,2,function(x) {
 
 tamp <- TAMPOR(dat, traits, noGIS = FALSE, useAllNonGIS = FALSE, batchPrefixInSampleNames = TRUE, GISchannels=c("126","131C"), iterations = 250,
        samplesToIgnore = FALSE, meanOrMedian = "median", removeGISafter = FALSE, minimumBatchSize = 5, parallelThreads=2, outputSuffix = "TAMPOR", 
-       path="/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized")
+       path="/path/to/Data/Protein/TamporNormalized")
   
   
   
