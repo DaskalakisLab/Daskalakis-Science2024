9ac8ccb5ab03a2d2b0f60451ccc5cd697d365cd9
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 20:18:15 -0400

    Update reformatNormData.R

--- Code/Protein_Y12_Y3/Peptides/Y1Y2/reformatNormData.R
+++ Code/Protein_Y12_Y3/Peptides/Y1Y2/reformatNormData.R
@@ -1,41 +1,41 @@
-anno <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_batch1.RDS")
+anno <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_batch11.RDS")
 
 prepare_norm <- function(norm, anno){
   norm <- as.data.frame(norm)
   
-  batch_designs <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/Proteomics/batch_designs.RDS")
+  batch1_designs <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/Proteomics/batch1_designs.RDS")
   
-  batch_designs <- as.data.frame(batch_designs)
-  batch_designs$Plate <- gsub("F", "", batch_designs$Plate)
-  batch_designs$Plate <- sprintf("%02d", as.numeric(batch_designs$Plate))
-  batch_designs$ID <- paste0("b",batch_designs$Plate, ".", batch_designs$Position)
+  batch1_designs <- as.data.frame(batch1_designs)
+  batch1_designs$Plate <- gsub("F", "", batch1_designs$Plate)
+  batch1_designs$Plate <- sprintf("%02d", as.numeric(batch1_designs$Plate))
+  batch1_designs$ID <- paste0("b",batch1_designs$Plate, ".", batch1_designs$Position)
   
-  batch_designs <- merge(batch_designs, anno[,c("BrNum","SampleID","Brain_Region_Protein")], by.x=c("BrNum", "Brain_Region"), by.y=c("BrNum", "Brain_Region_Protein"), all.x=T)
-  batch_designs[batch_designs$BrNum=="GIS","SampleID"] <- batch_designs[batch_designs$BrNum=="GIS","ID"]
+  batch1_designs <- merge(batch1_designs, anno[,c("BrNum","SampleID","Brain_Region_Protein")], by.x=c("BrNum", "Brain_Region"), by.y=c("BrNum", "Brain_Region_Protein"), all.x=T)
+  batch1_designs[batch1_designs$BrNum=="GIS","SampleID"] <- batch1_designs[batch1_designs$BrNum=="GIS","ID"]
   
   for (id in names(norm)){
     print(id)
-    names(norm)[names(norm)==id] <- batch_designs[batch_designs$ID == id,"BrNum"]
+    names(norm)[names(norm)==id] <- batch1_designs[batch1_designs$ID == id,"BrNum"]
   }
   names(norm)
   norm <- norm[,startsWith(names(norm), "B")]
   return(norm)
 }
 
-batch1_DG <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1/Peptides/batch1_Hippocampus.RDS")
-batch1_DG_clean <- as.data.frame(batch1_DG$cleanDat)
-batch1_DG_clean <- prepare_norm(batch1_DG_clean,anno)
-saveRDS(batch1_DG_clean, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1/Peptides/CleanWithSampleIDs/batch1_DG_clean_no_GIS.RDS")
+batch11_DG <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch11/Peptides/batch11_Hippocampus.RDS")
+batch11_DG_clean <- as.data.frame(batch11_DG$cleanDat)
+batch11_DG_clean <- prepare_norm(batch11_DG_clean,anno)
+saveRDS(batch11_DG_clean, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch11/Peptides/CleanWithSampleIDs/batch11_DG_clean_no_GIS.RDS")
 
-batch1_CA <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1/Peptides/batch1_Amygdala.RDS")
-batch1_CA_clean <- as.data.frame(batch1_CA$cleanDat)
-batch1_CA_clean <- prepare_norm(batch1_CA_clean,anno)
-saveRDS(batch1_CA_clean, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1/Peptides/CleanWithSampleIDs/batch1_CentralAmyg_clean_no_GIS.RDS")
+batch11_CA <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch11/Peptides/batch11_Amygdala.RDS")
+batch11_CA_clean <- as.data.frame(batch11_CA$cleanDat)
+batch11_CA_clean <- prepare_norm(batch11_CA_clean,anno)
+saveRDS(batch11_CA_clean, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch11/Peptides/CleanWithSampleIDs/batch11_CentralAmyg_clean_no_GIS.RDS")
 
-batch1_mPFC <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1/Peptides/batch1_mPFC.RDS")
-batch1_mPFC_clean <- as.data.frame(batch1_mPFC$cleanDat)
-batch1_mPFC_clean <- prepare_norm(batch1_mPFC_clean,anno)
-saveRDS(batch1_mPFC_clean, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch1/Peptides/CleanWithSampleIDs/batch1_mPFC_clean_no_GIS.RDS")
+batch11_mPFC <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch11/Peptides/batch11_mPFC.RDS")
+batch11_mPFC_clean <- as.data.frame(batch11_mPFC$cleanDat)
+batch11_mPFC_clean <- prepare_norm(batch11_mPFC_clean,anno)
+saveRDS(batch11_mPFC_clean, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/batch11/Peptides/CleanWithSampleIDs/batch11_mPFC_clean_no_GIS.RDS")
 
 
 
