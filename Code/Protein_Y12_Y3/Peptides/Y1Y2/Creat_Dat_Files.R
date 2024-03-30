4f501f05daa01b49467967bd4d9ba6c82035aae3
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-29 20:18:56 -0400

    Update Creat_Dat_Files.R

--- Code/Protein_Y12_Y3/Peptides/Y1Y2/Creat_Dat_Files.R
+++ Code/Protein_Y12_Y3/Peptides/Y1Y2/Creat_Dat_Files.R
@@ -7,13 +7,13 @@ library(doParallel)
 library(ggplot2)
 library(ggpubr)
 
-dat <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/1b.PD.TMT_Peptides_batch1.NoCorrection.RDS")
+dat <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/TamporNormalized/1b.PD.TMT_Peptides_batch11.NoCorrection.RDS")
 dat <- as.matrix(dat)
-traits <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/Proteomics/batch_designs.RDS")
+traits <- readRDS("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Protein/Proteomics/batch1_designs.RDS")
 traits$Plate <- gsub("F", "", traits$Plate)
 traits$Plate <- sprintf("%02d", as.numeric(traits$Plate))
 rownames(traits) <- paste0("b",traits$Plate, ".", traits$Position)
-names(traits)[names(traits)=="Plate"] <- "Batch"
+names(traits)[names(traits)=="Plate"] <- "batch1"
 
 
 traits_Amygdala <- traits[(traits$Brain_Region=="Amygdala") | (traits$Brain_Region=="GIS"),]
