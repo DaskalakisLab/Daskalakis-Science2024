175722a1a9b51c88cacc437711892ec1d27c02ae
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-30 10:50:08 -0400

    Update Limma.R

--- Code/Methylation/Limma.R
+++ Code/Methylation/Limma.R
@@ -1,12 +1,12 @@
-source("/data/humgen/daskalakislab/dipietro/SciencePaper/Code/Methylation/Limma_Functions.R")
+source("/path/to/Limma_Functions.R")
 
-sink("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Methylation/logbatch1.txt")
+sink("/path/to/logbatch1.txt")
 
 regions <- c("CentralAmyg", "DG", "mPFC")
 
 for (region in regions){
-  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_batch1.RDS", 
-                expr_file = paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Methylation/AartiNormalized/batch1/",region,"Mfunnorm.RDS"), 
+  limma_wrapper(anno_file = "/path/to/Annotation_batch1.RDS", 
+                expr_file = paste0("/path/to/batch1/",region,"Mfunnorm.RDS"), 
                 region = region, 
                 dx="PTSD", 
                 ptsd_vs_mdd = F, 
@@ -14,11 +14,11 @@ for (region in regions){
                 type = "meth",
                 covariates = c("PTSD","AgeDeath","PMI","Sex","ancestryPC1","ancestryPC2","Meth_PC1","Meth_PC2","Smoking","NeuN_neg","NeuN_pos"), 
                 contrast_var = "PTSD", 
-                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Methylation/batch1/PTSD/")
+                out_folder = "/path/to/batch1/PTSD/")
   
   
-  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_batch1.RDS", 
-                expr_file = paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Methylation/AartiNormalized/batch1/",region,"Mfunnorm.RDS"), 
+  limma_wrapper(anno_file = "/path/to/Annotation_batch1.RDS", 
+                expr_file = paste0("/path/to/batch1/",region,"Mfunnorm.RDS"), 
                 region = region, 
                 dx="MDD", 
                 ptsd_vs_mdd = F, 
@@ -26,10 +26,10 @@ for (region in regions){
                 type = "meth",
                 covariates = c("MDD","AgeDeath","PMI","Sex","ancestryPC1","ancestryPC2","Meth_PC1","Meth_PC2","Smoking","NeuN_neg","NeuN_pos"), 
                 contrast_var = "MDD", 
-                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Methylation/batch1/MDD/")
+                out_folder = "/path/to/batch1/MDD/")
   
-  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_batch1.RDS",
-                expr_file = paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Methylation/AartiNormalized/batch1/",region,"Mfunnorm.RDS"),
+  limma_wrapper(anno_file = "/path/to/Annotation_batch1.RDS",
+                expr_file = paste0("/path/to/batch1/",region,"Mfunnorm.RDS"),
                 region = region,
                 dx=NULL,
                 ptsd_vs_mdd = F,
@@ -37,10 +37,10 @@ for (region in regions){
                 type = "meth",
                 covariates = c("PTSD_MDD","AgeDeath","PMI","Sex","ancestryPC1","ancestryPC2","Meth_PC1","Meth_PC2","Smoking","NeuN_neg","NeuN_pos"),
                 contrast_var = "PTSD_MDD",
-                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Methylation/batch1/PTSD_MDD/")
+                out_folder = "/path/to/batch1/PTSD_MDD/")
 
-  limma_wrapper(anno_file = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Annotation/Annotation_batch1.RDS",
-                expr_file = paste0("/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Methylation/AartiNormalized/batch1/",region,"Mfunnorm.RDS"),
+  limma_wrapper(anno_file = "/path/to/Annotation_batch1.RDS",
+                expr_file = paste0("/path/to/batch1/",region,"Mfunnorm.RDS"),
                 region = region,
                 dx=NULL,
                 ptsd_vs_mdd = T,
@@ -48,6 +48,6 @@ for (region in regions){
                 type = "meth",
                 covariates = c("PTSD","AgeDeath","PMI","Sex","ancestryPC1","ancestryPC2","Meth_PC1","Meth_PC2","Smoking","NeuN_neg","NeuN_pos"),
                 contrast_var = "PTSD",
-                out_folder = "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/LimmaResults/Methylation/batch1/PTSD_vs_MDD/")
+                out_folder = "/path/to/batch1/PTSD_vs_MDD/")
 }
 sink()
