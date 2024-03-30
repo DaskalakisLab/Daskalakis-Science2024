796ee796643cd4d6eddb3113d018f240114280ee
Ioulia Tsatsani <46993370+iouliadt@users.noreply.github.com>
2024-03-30 10:56:58 -0400

    Update createCPGMap.R

--- Code/Methylation/createCPGMap.R
+++ Code/Methylation/createCPGMap.R
@@ -14,7 +14,7 @@ library(stringr)
 annEPIC <- getAnnotation(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
 df <- as.data.frame(annEPIC)
 df <- df[,c("Name", "UCSC_RefGene_Name")]
-#saveRDS(df, "/data/humgen/daskalakislab/dipietro/SciencePaper/Data/Methylation/CPGMap/cpgMapFull.RDS")
+#saveRDS(df, "/path/to/Methylation/CPGMap/cpgMapFull.RDS")
 df$UCSC_RefGene_Name <- gsub(";.*", "", df$UCSC_RefGene_Name)
 df[df$UCSC_RefGene_Name=="", "UCSC_RefGene_Name"] <- df[df$UCSC_RefGene_Name=="", "Name"]
 names(df) <- c("cpg", "symbol")
